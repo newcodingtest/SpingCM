e866f2dc126fe264f092478992d06f2b0a072d7b
윤주영 <pulpul8282@naver.com>
2021-05-13 15:08:32 +0900

    [스프링 레거시 게시판]

---
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,174 @@
+package org.zerock.controller;
+
+import java.nio.file.Files; 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardAttachVO;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.PageDTO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+
+	private BoardService service;
+
+	
+	//글 등록페이지로 이동
+	@GetMapping("/register")
+	@PreAuthorize("isAuthenticated()")
+	public void register() {
+
+	}
+
+
+	//글 리스트(cir: 페이징 객체)
+	@GetMapping("/list")
+	public void list(Criteria cri, Model model) {
+
+		log.info("list: " + cri);
+		model.addAttribute("list", service.getList(cri));
+		// model.addAttribute("pageMaker", new PageDTO(cri, 123));
+
+		int total = service.getTotal(cri);
+
+		log.info("total: " + total);
+
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
+
+	}
+
+	//글 등록하기 조건: 로그인 된 상태
+	@PostMapping("/register")
+	@PreAuthorize("isAuthenticated()")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+
+		log.info("==========================");
+
+		log.info("register: " + board);
+
+		if (board.getAttachList() != null) {
+
+			board.getAttachList().forEach(attach -> log.info(attach));
+
+		}
+
+		log.info("==========================");
+
+		service.register(board);
+
+		rttr.addFlashAttribute("result", board.getBno());
+
+		return "redirect:/board/list";
+	}
+
+	//글 조회 및 수정 (넘기는 값 글번호, 페이징 객체)
+	@GetMapping({ "/get", "/modify" })
+	public void get(@RequestParam("bno") Long bno, @ModelAttribute("cri") Criteria cri, Model model) {
+
+		log.info("/get or modify");
+		model.addAttribute("board", service.get(bno));
+	}
+
+
+	//글 수정 조건: 작성자==로그인id principal은 UserDetailsService 사용자정보랑 같음
+	@PreAuthorize("principal.username == #board.writer")
+	@PostMapping("/modify")
+	public String modify(BoardVO board, Criteria cri, RedirectAttributes rttr) {
+		log.info("modify:" + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list" + cri.getListLink();
+	}
+	
+	
+
+	//글 삭제 조건: 작성자==로그인id
+	@PreAuthorize("principal.username == #writer")
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, Criteria cri, RedirectAttributes rttr, String writer) {
+
+		log.info("remove..." + bno);
+
+		List<BoardAttachVO> attachList = service.getAttachList(bno);
+
+		if (service.remove(bno)) {
+
+			// delete Attach Files
+			deleteFiles(attachList);
+
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list" + cri.getListLink();
+	}
+
+	//파일 삭제 메서드
+	private void deleteFiles(List<BoardAttachVO> attachList) {
+
+		if (attachList == null || attachList.size() == 0) {
+			return;
+		}
+
+		log.info("delete attach files...................");
+		log.info(attachList);
+		
+		//파일 여러개 첨부가능 하니 삭제도 가능
+		attachList.forEach(attach -> {
+			try {
+				Path file = Paths.get(
+						"C:\\upload\\" + attach.getUploadPath() + "\\" + attach.getUuid() + "_" + attach.getFileName());
+
+				Files.deleteIfExists(file);
+
+				//파일이 이미지인경우 썸네일 파일(_s 로 구분)까지 삭제해줘야함 총 2개삭제
+				if (Files.probeContentType(file).startsWith("image")) {
+
+					Path thumbNail = Paths.get("C:\\upload\\" + attach.getUploadPath() + "\\s_" + attach.getUuid() + "_"
+							+ attach.getFileName());
+
+					Files.delete(thumbNail);
+				}
+
+			} catch (Exception e) {
+				log.error("delete file error" + e.getMessage());
+			} // end catch
+		});// end foreachd
+	}
+
+	//파일정보 가져오기(uuid, uploadpath, filename, filetype, bno)
+	@GetMapping(value = "/getAttachList", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<BoardAttachVO>> getAttachList(Long bno) {
+
+		log.info("getAttachList " + bno);
+
+		return new ResponseEntity<>(service.getAttachList(bno), HttpStatus.OK);
+
+	}
+
+}
---
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -0,0 +1,279 @@
+package org.zerock.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
+
+import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
+
+@Controller
+@Log4j
+public class UploadController {
+
+	@GetMapping("/uploadForm")
+	public void uploadForm() {
+
+		log.info("upload form");
+	}
+
+	//클라이언트 생성한 파일----->서버 업로드 하여 서버에 파일생성
+	@PostMapping("/uploadFormAction")
+	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+
+		//파일생성될 최초 루트폴더 위치
+		String uploadFolder = "C:\\upload";
+
+		for (MultipartFile multipartFile : uploadFile) {
+
+			log.info("-------------------------------------");
+			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size: " + multipartFile.getSize());
+
+			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			} // end catch
+		} // end for
+
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/uploadAjax")
+	public void uploadAjax() {
+
+		log.info("upload ajax");
+	}
+
+
+	//날짜별 폴더 생성 메서드
+	private String getFolder() {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+		Date date = new Date();
+
+		String str = sdf.format(date);
+
+		return str.replace("-", File.separator);
+	}
+
+
+	//이미지 타입 체크 메서드
+	private boolean checkImageType(File file) {
+
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+
+			return contentType.startsWith("image");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+
+	//파일 업로드-->날짜별 폴더가 생성되고 그에 안에 파일이 저장됨
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+
+		List<AttachFileDTO> list = new ArrayList<>();
+		String uploadFolder = "C:\\upload";
+
+		//날짜폴더 생성
+		String uploadFolderPath = getFolder();
+		// make folder --------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+
+		
+		//폴더경로가 존재하지 않는다면 생성-->주로 다음날일경우
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// make yyyy/MM/dd folder
+
+		for (MultipartFile multipartFile : uploadFile) {
+
+			AttachFileDTO attachDTO = new AttachFileDTO();
+
+			String uploadFileName = multipartFile.getOriginalFilename();
+
+			// IE has file path
+			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
+			log.info("only file name: " + uploadFileName);
+			attachDTO.setFileName(uploadFileName);
+
+			UUID uuid = UUID.randomUUID();
+
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+
+			try {
+				File saveFile = new File(uploadPath, uploadFileName);
+				multipartFile.transferTo(saveFile);
+
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+
+				// 이미지 파일 확인--->이미지 파일이면 썸네일 파일 1개 추가생성
+				if (checkImageType(saveFile)) {
+
+					attachDTO.setImage(true);
+
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+
+					thumbnail.close();
+				}
+
+				list.add(attachDTO);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} // end for
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	
+	//클라이언트-->서버로 업로드 된 사진 보여주기 -->브라우저에게 content-Type 으로 파일주소값전송
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+
+		log.info("fileName: " + fileName);
+
+		File file = new File("c:\\upload\\" + fileName);
+
+		log.info("file: " + file);
+
+		ResponseEntity<byte[]> result = null;
+
+		try {
+			HttpHeaders header = new HttpHeaders();
+
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	
+	//업로드 파일 다운로드(User-Agent로 브라우저 마다 다운법 구별)
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String fileName) {
+
+		Resource resource = new FileSystemResource("c:\\upload\\" + fileName);
+
+		if (resource.exists() == false) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		String resourceName = resource.getFilename();
+		
+		
+		//uuid 추출
+		// _ 로 uuid구별 ex) 621487-1dssfda_주영.jpg  --->'uuid'+ _ + '파일실제이름'+.jpg 
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+		HttpHeaders headers = new HttpHeaders();
+		try {
+
+			//인터넷익스폴로어 ie
+			boolean checkIE = (userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1);
+
+			String downloadName = null;
+
+			//브라우저 마다 다운 정책이 다르기에 구분
+			if (checkIE) {
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF8").replaceAll("\\+", " ");
+			} else {
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+
+			headers.add("Content-Disposition", "attachment; filename=" + downloadName);
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
+	
+
+	//파일삭제  조건: 로그인된 상태
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/deleteFile")
+	@ResponseBody
+	public ResponseEntity<String> deleteFile(String fileName, String type) {
+
+		log.info("deleteFile: " + fileName);
+
+		File file;
+
+		try {
+			file = new File("c:\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));
+
+			file.delete();
+
+			//파일타입이 image일 경우 썸네일 파일까지 총 2개 삭제진행
+			if (type.equals("image")) {
+
+				String largeFileName = file.getAbsolutePath().replace("s_", "");
+
+				log.info("largeFileName: " + largeFileName);
+
+				file = new File(largeFileName);
+
+				file.delete();
+			}
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+
+	}
+	
+
+}
